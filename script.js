35f14bf455064b60c2186dde8cb4f24b703926da
Anel Poturovic <Anel@Anels-MacBook-Pro.local>
2018-10-22 11:37:34 +0200

    added Logout function

--- script.js
+++ script.js
@@ -10,9 +10,24 @@ $(document).ready(function() {
 
         if (ValidUsername === true && ValidPassword === true) { // if ValidEmail & ValidPassword
             $('.LoggedIn').css('display', 'block');
+            $('.NotLoggedIn').hide();
+            $("#logout").css('visibility', 'visible');
+            $("#submit").css('visibility', 'hidden');
+            
         }
         else {
             $('.NotLoggedIn').css('display', 'block'); // show error msg
+            $('.LoggedIn').hide();
+            $("#logout").css('visibility' , 'hidden');
+            $("#submit").css('visibility' , 'visible');
+            
         }
     });
+
+
+    $('#logout').click(function() {
+        window.location.href ="index.html";
+    });
+
+
 });
